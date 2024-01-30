7e935088ec79e89206545d8b3829097c0c54a567
Ismail Karabulut <i.karabulut93@gmail.com>
2024-01-30 00:38:00 -0600

    Initial pass at passing and encrypting a secret
    This will take a plain text string from a POST request, create an MD5 hash and ssave the hash:plain_text mapping to JSON

--- main.go
+++ main.go
@@ -1,34 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
-    "errors"
+	"strings"
 
-    "github.com/ikarabulut/hidenseek/server"
+	"github.com/ikarabulut/hidenseek/server"
 )
 
-
 func main() {
-    dataFilePath := os.Getenv("DATA_FILE_PATH"); if dataFilePath == "" {
-        fmt.Println("You have not specified a DATA_FILE_PATH, a default secrets file will be created in your applications root")
-        dataFilePath = "./secrets.json"
-    }
+	dataFilePath := os.Getenv("DATA_FILE_PATH")
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	if dataFilePath == "" {
+		fmt.Println("You have not specified a DATA_FILE_PATH, a default secrets file will be created in your applications root")
+		dataFilePath = "/secrets.json"
+		var sb strings.Builder
+		sb.WriteString(wd)
+		sb.WriteString(dataFilePath)
+		dataFilePath = sb.String()
+	}
 
-    handleFilePath(dataFilePath)
+	handleFilePath(dataFilePath)
 
-    server.RunServer()
+	server.RunServer(dataFilePath)
 }
 
-func handleFilePath(filePath string) (secretsFile os.FileInfo, err error){
-    file, err := os.Stat(filePath); if errors.Is(err, os.ErrNotExist) {
-        file, err := createFile(filePath)
-        return file, err
-    }
-    return file, err
+func handleFilePath(filePath string) (secretsFile os.FileInfo, err error) {
+	file, err := os.Stat(filePath)
+	if errors.Is(err, os.ErrNotExist) {
+		file, err := createFile(filePath)
+		return file, err
+	}
+	return file, err
 }
 
-func createFile(filePath string) (file os.FileInfo, err error){
-    os.Create(filePath)
-    return
+func createFile(filePath string) (file os.FileInfo, err error) {
+	os.Create(filePath)
+	return
 }
--- server/run.go
+++ server/run.go
@@ -1,35 +1,87 @@
 package server
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 
 	"github.com/ikarabulut/hidenseek/util"
 )
 
+type FileStore struct {
+	mu              sync.Mutex
+	secretsFilePath string
+	Store           map[string]string
+}
 
+func (c *FileStore) updateMap(secret string, hash string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	f, err := os.Open(c.secretsFilePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	jsonData, err := io.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if len(jsonData) != 0 {
+		json.Unmarshal(jsonData, &c.Store)
+	}
+
+	c.Store[hash] = secret
+	j, err := json.Marshal(c.Store)
+
+	f, err = os.Create(c.secretsFilePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	_, err = f.Write(j)
+
+}
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	BuildResponse(w, 200, "health-check")
 	return
 }
 
-func SecretHandler(w http.ResponseWriter, r *http.Request) {
-	requestModel := ParseBody(r)
-	secretHex := util.CreateMd5Hex(requestModel.PlainText)
-	BuildResponse(w, 200, secretHex)
-	return
+func SecretHandler(secretsFilePath string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		file, err := os.Open(secretsFilePath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		fileC := FileStore{
+			mu:              sync.Mutex{},
+			secretsFilePath: secretsFilePath,
+			Store:           make(map[string]string),
+		}
+
+		requestModel := ParseBody(r)
+		secretHex := util.CreateMd5Hex(requestModel.PlainText)
+		fileC.updateMap(requestModel.PlainText, secretHex)
+		BuildResponse(w, 200, secretHex)
+	}
 }
 
-func RunServer() {
+func RunServer(secretsFilePath string) {
 	mux := http.NewServeMux()
 
 	health := http.HandlerFunc(HealthCheckHandler)
-	secret := http.HandlerFunc(SecretHandler)
+	secret := http.HandlerFunc(SecretHandler(secretsFilePath))
 
 	mux.Handle("/health", health)
 	mux.Handle("/secret", secret)
 
 	log.Print("Listening...")
 	http.ListenAndServe(":3000", mux)
-}	
+}
